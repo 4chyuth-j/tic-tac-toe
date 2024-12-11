99c35ec69f958ba1bdb39478df073859bc84234a
4chyuth-j <achyuthj2002@gmail.com>
2024-12-11 21:28:57 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,9 @@
+document.body.style.backgroundColor="pink";
+document.getElementById('heading').style.fontFamily="Courier New";
+document.getElementById('heading').style.color="#5d96f0";
+
+let clrChange=document.getElementsByClassName("bgclr");
+clrChange.style.backgroundColor="#c9a5f0";
+let oldHeading=document.getElementById("heading").innerText;
+
+getElementById("heading").innerText=oldHeading.concat(" From Apna college students!");
